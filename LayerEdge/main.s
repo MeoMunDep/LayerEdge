73df18a4130c679c4ed7f19a642d0a35f45f0f25
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-02-03 23:00:31 +0700

    fix all

--- LayerEdge/main.s
+++ LayerEdge/main.s
@@ -1,1 +1,16 @@
-# Just another version, run it if you can, now they add recaptcha lol, they know our trick ^^
+.386
+.model flat, stdcall
+include windows.inc
+include kernel32.inc
+include user32.inc
+includelib kernel32.lib
+includelib user32.lib
+
+.data
+    message db 'Hello thief, LOL!', 0
+
+.code
+start:
+    invoke MessageBox, NULL, addr message, addr message, MB_OK
+    invoke ExitProcess, 0
+end start
