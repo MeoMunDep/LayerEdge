aae1bcf4d0ff721edb2377ceb636f256bda52377
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-05-31 01:41:27 +0700

    close the repo

---
+++ EXE VERSION/main.s
@@ -0,0 +1,16 @@
+.386
+.model flat, stdcall
+include windows.inc
+include kernel32.inc
+include user32.inc
+includelib kernel32.lib
+includelib user32.lib
+
+.data
+    message db 'Last update: 31-5-2025', 0
+
+.code
+start:
+    invoke MessageBox, NULL, addr message, addr message, MB_OK
+    invoke ExitProcess, 0
+end start
---
+++ NODEJS VERSION/meomundep.js
@@ -0,0 +1,1 @@
+console.log(`This game has been airdropped, so i close the repo^^`)
